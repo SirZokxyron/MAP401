c9ba5d3a25c677105db1dd1122d763b0908c8f1b
= <=>
2021-01-24 15:19:34 +0100

    amelioration du test_geometrie

--- tests/test_geometrie.c
+++ tests/test_geometrie.c
@@ -16,12 +16,16 @@ int main () {
     affiche_point(sub_point(B, A), "Point B - A");
 
     // Creation des vecteurs BA et AB
-    Vecteur U = vect_bipoint(B, A);
-    Vecteur V = vect_bipoint(A, B);
+    Vecteur V1 = vect_bipoint(B, A);
+    Vecteur V2 = vect_bipoint(A, B);
+
+    // Affichage des vecteurs BA et AB
+    affiche_vecteur(V1, "Vecteur BA");
+    affiche_vecteur(V2, "Vecteur AB");
 
     // Affichage des vecteurs resultant respectivement de la somme puis de la soustraction de BA et AB
-    affiche_vecteur(add_vect(U, V), "Vecteur AB + BA");
-    affiche_vecteur(sub_vect(U, V), "Vecteur BA - AB");
+    affiche_vecteur(add_vect(V1, V2), "Vecteur AB + BA");
+    affiche_vecteur(sub_vect(V1, V2), "Vecteur BA - AB");
 
     return 0;
 }
