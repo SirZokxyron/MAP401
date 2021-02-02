88b25907ff2997d2eca44dec17cfb1295ecce7f1
Hugo 'SirZokxyron' Bensbia <sir.zokxyron@gmail.com>
2021-02-02 18:39:41 +0100

    fixed build folder deletion

--- scripts/[utile]_clean_build.sh
+++ scripts/[utile]_clean_build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 echo "---------------------------------------------"
+echo "         Nettoyage de build/autres           "
+echo "---------------------------------------------"
+rm build/autres/[!.]*
+echo "---------------------------------------------"
 echo "         Nettoyage de build/tache_1          "
 echo "---------------------------------------------"
-rm build/tache_1/*
+rm build/tache_1/[!.]*
 echo "---------------------------------------------"
 echo "         Nettoyage de build/tache_3          "
 echo "---------------------------------------------"
-rm build/tache_3/*
+rm build/tache_3/[!.]*
 echo "---------------------------------------------"
 echo "         Nettoyage de build/tache_4          "
 echo "---------------------------------------------"
-rm build/tache_4/*
+rm build/tache_4/[!.]*
+echo "---------------------------------------------"
+echo "         Nettoyage de build/tache_5          "
+echo "---------------------------------------------"
+rm build/tache_5/[!.]*
