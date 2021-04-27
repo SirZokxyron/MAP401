2d9e5496241e319a5876e7ba75c13ba1caf6ed8d
SirZokxyron <sir.zokxyron@gmail.com>
2021-04-27 09:06:41 +0200

    commented test_distance

--- tests/test_distance.c
+++ tests/test_distance.c
@@ -3,6 +3,8 @@
 int main () {
 
     reel x, y;
+
+    //> Initialisation du point P
     printf("Point P :\n");
     printf("\tx = ");
     scanf("%lf%*c", &x);
@@ -10,6 +12,7 @@ int main () {
     scanf("%lf%*c", &y);
     Point P = set_point(x, y);
 
+    //> Initialisation du point A
     printf("Point A :\n");
     printf("\tx = ");
     scanf("%lf%*c", &x);
@@ -17,6 +20,7 @@ int main () {
     scanf("%lf%*c", &y);
     Point A = set_point(x, y);
 
+    //> Initialisation du point B
     printf("Point B :\n");
     printf("\tx = ");
     scanf("%lf%*c", &x);
@@ -24,7 +28,7 @@ int main () {
     scanf("%lf%*c", &y);
     Point B = set_point(x, y);
 
-    //> Test du calcul de la distance segment - point
+    //> Test du calcul de la distance [A, B] et P
     printf("Distance [A, B] et P : %lf\n", dist_vect_point(A, B, P));
 
     return 0;
