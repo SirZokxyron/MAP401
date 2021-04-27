2c06036544843731ac92e2f323e199e578a21efb
SirZokxyron <sir.zokxyron@gmail.com>
2021-04-27 08:37:06 +0200

    commented contour package

--- interfaces/contours.h
+++ interfaces/contours.h
@@ -8,7 +8,7 @@
 
 /* Fonctions pour determiner le contour d'une image */
 
-    //? Renvoie le pixel de depart, le premier pixel noir trouve dans l'image masque
+    //? [ANCIENNE] Renvoie le pixel de depart, le premier pixel noir trouve dans l'image masque
     /* Point pos_depart(Image I); */
 
     //* Creer le nom de fichier correct pour la sauvegarde du .contours d'une image
@@ -17,13 +17,17 @@
     //* Affiche a l'ecran le nombre de contours et de segments totaux
     void debug_contour(Image I);
 
-    //* Affiche a l'ecran le nombre de bezier2
+    //* Affiche a l'ecran le nombre de courbe de bezier de degre 2
     void debug_bezier2(Image I);
 
-    //* Affiche a l'ecran le nombre de bezier3
+    //* Affiche a l'ecran le nombre de courbe de bezier de degre 3
     void debug_bezier3(Image I);
 
     //* Renvoie le contour d'une image passee en argument selon l'algorithme vu en cours
+    //* 0 - Aucune simplification
+    //* 1 - Simplification par distance-seuil
+    //* 2 - Simplification par courbe de bezier de degre 2
+    //* 3 - Simplification par courbe de bezier de degre 3
     void determiner_contour(Image I, int simplification);
 
     //* Sauvegarde un contour donne sous forme de liste de point dans un fichier .contours
--- lib/contours.c
+++ lib/contours.c
@@ -21,20 +21,20 @@
     //* Creer le nom de fichier correct pour la sauvegarde du .contours d'une image
     string get_fichier_contours(string nom) {
         
-        //> Ajout du suffix ".contours" a la fin du nom
-        string suffix = ".contours";
-        string temp = (string)malloc(1 + strlen(nom) + strlen(suffix));
-        sprintf(temp, "%s%s", nom, suffix);
+        //> Ajout du suffixe ".contours" a la fin du nom
+        string suffixe = ".contours";
+        string temp = (string)malloc(1 + strlen(nom) + strlen(suffixe));
+        sprintf(temp, "%s%s", nom, suffixe);
 
-        //> Ajout du prefix "build/" au debut du nom
-        string prefix = "build/";
-        string nom_f = (string)malloc(1 + strlen(prefix) + strlen(temp));
-        sprintf(nom_f, "%s%s", prefix, temp);
+        //> Ajout du prefixe "build/" au debut du nom
+        string prefixe = "build/";
+        string nom_f = (string)malloc(1 + strlen(prefixe) + strlen(temp));
+        sprintf(nom_f, "%s%s", prefixe, temp);
 
         return nom_f;
     }
 
-    //* Affiche a l'ecran le nombre de contours et de segments totaux
+    //* Affiche a l'ecran le nombre de contours et de segments totaux (pour une simpli par distance seuil)
     void debug_contour(Image I) {
         
         //> Recuperation du fichier .contours correspond a l'image
@@ -68,7 +68,7 @@
         fclose(f);
     }
 
-    //* Affiche a l'ecran le nombre de contours et de segments totaux
+    //* Affiche a l'ecran le nombre de contours et de segments totaux (pour une simpli par courbe de bezier 2)
     void debug_bezier2(Image I) {
         
         //> Recuperation du fichier .contours correspond a l'image
@@ -101,7 +101,7 @@
         fclose(f);
     }
 
-    //* Affiche a l'ecran le nombre de contours et de segments totaux
+    //* Affiche a l'ecran le nombre de contours et de segments totaux (pour une simpli par courbe de bezier 3)
     void debug_bezier3(Image I) {
         
         //> Recuperation du fichier .contours correspond a l'image
@@ -211,17 +211,20 @@
     //* Sauvegarde un contour donne sous forme de liste de point dans un fichier .contours
     void save_contour(liste L, char * nom)  {
 
+        //> On ouvre le fichier .contours passe en argument
         FILE *f;
         f = fopen(nom, "r+");
 
-        //> Si f == NULL, alors on cree le fichier contenant le premier contour
+        //> Si f == NULL, alors on cree le fichier qui va contenir le premier contour
         if (!f) {
             //! fclose(f);          //! Ne pas fclose un pointeur vers un fichier NULL
             f = fopen(nom, "w");
             fprintf(f, "1");
             fprintf(f, "                 ");
             fprintf(f, "\n\n");
-        } else {
+        } 
+        //> Si f existe deja, alors on le met simplement a jour l'entete du fichier
+        else {
             int nb_contours;
             fscanf(f, "%d", &nb_contours);
             nb_contours++;
@@ -231,6 +234,7 @@
             fseek(f, 0, SEEK_END);
         }
 
+        //> On ecrit le nouveau contour dans le fichier
         int tailleL = L.taille;
         fprintf(f, "%d\n", tailleL);
         cellule * cell = L.tete;
@@ -240,5 +244,6 @@
         }
         fprintf(f, "\n");
 
+        //> On ferme le fichier
         fclose(f);
     }
